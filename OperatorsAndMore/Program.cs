fbb237fbfa2674faf5230cb1a3e5ba268e9eb965
Carl <Carl@LAPTOP-ENVY-I76>
2017-09-04 07:23:12 +0200

    Point etc

--- OperatorsAndMore/Program.cs
+++ OperatorsAndMore/Program.cs
@@ -1,46 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using CSharpFundamentals;
 
 namespace CSharpFundamentals
 
 {
-    class Program
+}
+class Program
     {
         static void Main(string[] args)
         {
-            var numbers = new List<int> { 1, 2, 3, 4, 5, 6 };
-            var lijst   =    new List<int> {1,2,3,4,5,6};
-            var smallest = getSmallest(numbers, 3);
-            foreach (var number in smallest)
-            {
-                Console.WriteLine(number);
-            }
+        var point = new Point(10,20);
+            
+        point.Move(5,4);
 
         }
 
-        public static List<int> getSmallest(List<int> list, int count)
-        {
-            var smallest = new List<int>();
-            while (smallest.Count< count)
-            {
-                var min = getSmallest(list);
-                smallest.Add(min);
-                list.Remove(min);
-            }
-            return smallest;
-        }
-
-        public static int getSmallest(List<int> list)
-        {
-            var min = list[0];
-            for (var i = 1; i < list.Count; i++)
-            {
-                if (list[i] > min)
-                     min = list[i];
-            }
-            return min;
-
-        }
+       
     }
 }
---
+++ OperatorsAndMore/numbersGetSmallest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OperatorsAndMore
+{
+    class numbersGetSmallest
+    {
+        public static List<int> getSmallest(List<int> list, int count)
+        {
+            var smallest = new List<int>();
+            while (smallest.Count < count)
+            {
+                var min = getSmallest(list);
+                smallest.Add(min);
+                list.Remove(min);
+            }
+            return smallest;
+        }
+
+        public static int getSmallest(List<int> list)
+        {
+            var min = list[0];
+            for (var i = 1; i < list.Count; i++)
+            {
+                if (list[i] > min)
+                    min = list[i];
+            }
+            return min;
+
+        }
+
+        static void Main(string[] args)
+        {
+            var numbers = new List<int> { 1, 2, 3, 4, 5, 6 };
+            var lijst = new List<int> { 1, 2, 3, 4, 5, 6 };
+            var smallest = getSmallest(numbers, 3);
+            foreach (var number in smallest)
+            {
+                Console.WriteLine(number);
+            }
+
+        }
+    }
+}
